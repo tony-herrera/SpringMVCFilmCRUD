c8f9234dcf1d3f1a51cc9d31c38ad0bbc4314a20
Stigmond <stigmond@gmail.com>
2020-10-16 18:52:21 -0600

    Initiation of servlet and home page

---
+++ src/main/java/com/skilldistillery/films/controllers/FilmController.java
@@ -0,0 +1,16 @@
+package com.skilldistillery.films.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class FilmController {
+	@RequestMapping(path = "index.do", method = RequestMethod.GET)
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("WEB-INF/home.jsp");
+		return mv;
+	}
+}
