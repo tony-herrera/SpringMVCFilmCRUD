db71d0f16937b95add28c746198d3ff92c6ef65a
Ashley Zink <pamperedzink@gmail.com>
2020-10-17 11:27:13 -0600

    updates and fixes

--- src/main/java/com/skilldistillery/films/dao/MVCFilmSiteDAOObject.java
+++ src/main/java/com/skilldistillery/films/dao/MVCFilmSiteDAOObject.java
@@ -1,4 +1,4 @@
-package com.skilldistillery.mvcfilmsiteproject.dao;
+package com.skilldistillery.films.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -10,26 +10,34 @@ import java.util.List;
 
 import org.springframework.stereotype.Component;
 
-import com.skilldistillery.film.entities.Actor;
-import com.skilldistillery.film.entities.Film;
+import com.skilldistillery.films.entities.Actor;
+import com.skilldistillery.films.entities.Film;
 
 @Component
 public class MVCFilmSiteDAOObject implements MVCFilmSiteDAO {
 
-	public static final String URL = "jdbc:mysql://localhost:3306/sdvid?useSSL=false";
-	
-	public MVCFilmSiteDAOObject() {
-		
+	public static final String URL = "jdbc:mysql://localhost:3306/sdvid?useSSL=false&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+	static {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	
+
+	public MVCFilmSiteDAOObject() {
+
+	}
+
 	@Override
 	public Film findFilmById(int filmId) {
-		
+
 		Film tempFilm = new Film();
 		String user = "student";
 		String pw = "student";
 		String sqltxt = "SELECT film.id, film.title, film.description, film.release_year, film.length, film.rating, film.special_features, language.name, category.name FROM film JOIN film_category ON film.id = film_category.film_id JOIN category ON category.id = film_category.category_id JOIN language ON film.language_id = language.id WHERE film.id = ?";
-		
+
 		try {
 			Connection conn = DriverManager.getConnection(URL, user, pw);
 			PreparedStatement stmt = conn.prepareStatement(sqltxt);
@@ -49,22 +57,22 @@ public class MVCFilmSiteDAOObject implements MVCFilmSiteDAO {
 				return null;
 			}
 			stmt.close();
-			conn.close();	
+			conn.close();
 		} catch (SQLException e) {
-		
+
 			e.printStackTrace();
 		}
 		return tempFilm;
 	}
 
 	public List<Actor> findActorsByFilmId(int filmId) {
-		
+
 		List<Actor> tempList = new ArrayList<>();
 		Actor tempActor;
 		String user = "student";
 		String pw = "student";
 		String sqltxt = "SELECT actor.id, actor.first_name, actor.last_name FROM actor JOIN film_actor ON actor.id = film_actor.actor_id JOIN film ON film.id = film_actor.film_id WHERE film.id = ?";
-		
+
 		try {
 			Connection conn = DriverManager.getConnection(URL, user, pw);
 			PreparedStatement stmt = conn.prepareStatement(sqltxt);
@@ -84,7 +92,7 @@ public class MVCFilmSiteDAOObject implements MVCFilmSiteDAO {
 			stmt.close();
 			conn.close();
 		} catch (SQLException e) {
-			
+
 			e.printStackTrace();
 		}
 		return tempList;
