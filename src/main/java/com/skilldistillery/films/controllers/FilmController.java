8edd85d89bbc48c58cb81da10a7b8c296c2ee562
Ashley Zink <pamperedzink@gmail.com>
2020-10-17 10:22:53 -0600

    find film by id controller request mapping complete

--- src/main/java/com/skilldistillery/films/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/films/controllers/FilmController.java
@@ -5,12 +5,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.skilldistillery.film.entities.Film;
+import com.skilldistillery.mvcfilmsiteproject.dao.MVCFilmSiteDAO;
+
 @Controller
 public class FilmController {
+	
+	private MVCFilmSiteDAO filmDAO;
+	
 	@RequestMapping(path = "index.do", method = RequestMethod.GET)
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("WEB-INF/home.jsp");
 		return mv;
 	}
+	
+	@RequestMapping(path = "index.do", method = RequestMethod.GET)
+	public ModelAndView findFilmById(int filmId) {
+		Film film = filmDAO.findFilmById(filmId);
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("film", film);
+		mv.setViewName("WEB-INF/SingleFilmResult.jsp");
+		return mv;
+	}
+	
+	
 }
